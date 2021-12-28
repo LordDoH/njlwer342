f4b0a09a72719f14563a61c00d1d19d08c6bece9
Daniel <dochoahenao@gmail.com>
2021-12-27 21:38:33 -0500

    Agregada funcionalidad de agregar tareas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState} from 'react';
 
 /// Modifica el componente para que se puedan agregar tareas
 
-class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
-          </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
-        </div>
-      </div>
-    )
+function App() {
+  const [list, changeList] = useState([
+    'Sacar la ropa', 
+    'Hacer la cama', 
+    'Leer un rato'
+  ]);
+  
+  const [newItem, setNewItem] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    newItem != '' ? changeList ([...list,newItem]):setNewItem ('');
+    setNewItem ('')
   }
+
+  const onChange = (e) => {
+    setNewItem (e.target.value)
+  }
+
+  return (
+    <div className='wrapper'>
+      <div className='list'>
+        <h3>Por hacer:</h3>
+        <ul className='todo'>
+          {list.map((e, index)=>{return <li key = {index}>{e}</li>})}
+        </ul>
+          <form 
+          onSubmit = {onSubmit}>
+            <input 
+            value={newItem}
+            type='text' 
+            id='new-task' 
+            placeholder='Ingresa una tarea y oprime Enter'
+            onChange={onChange}
+             />
+          </form>
+      </div>
+    </div>
+  )
+  
 }
 
 
